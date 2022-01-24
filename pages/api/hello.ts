e995b5f99e9ecc05a402b9e38b344a093ae86f5f
root <root@DESKTOP-SEI0E83.localdomain>
2022-01-24 22:14:37 +0100

    basic example configuration

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {PrismaClient} from '@prisma/client'
+
+const prisma = new PrismaClient();
 
 type Data = {
   name: string
+  test: string | undefined | null
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+  let testValue = allUsers.at(0)?.mail;
+  res.status(200).json({ name: 'John Doe2', test: testValue })
+
 }
+
